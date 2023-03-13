942a8182da172459a0cb69effd1327d2f628f4ef
Vasily Galkin <galkin-vv@ya.ru>
2023-03-13 02:52:56 +0000

    fix launchers called withput ext

--- Lib/idle.bat
+++ Lib/idle.bat
@@ -1,4 +1,4 @@
-@classmethod # 2>nul & (if not exist "%~dp0\..\python.exe" (echo Fatal ..\python.exe not found & pause) else (title %~0 & "%~dp0\..\python.exe" "%~0" %*)) & exit /B & # noqa: E501
+@classmethod # 2>nul & (if not exist "%~dp0\..\python.exe" (echo Fatal ..\python.exe not found & pause) else (title %~f0 & "%~dp0\..\python.exe" "%~f0" %*)) & exit /B & # noqa: E501
 def __unused(): "fake function to help writing header that allows executing same file as python and batch"
 
 import idlelib.pyshell
