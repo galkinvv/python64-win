35657045dcd9836c24f2de31028e75d1fc1d34a6
Vasily Galkin <galkin.vv@yandex.kz>
2024-12-27 04:31:25 +0300

    Setup env in python3.pth to make pip generating relative shebangs in exes

--- package-python.py.bat
+++ package-python.py.bat
@@ -1,7 +1,8 @@
 @classmethod # 2>nul & (if not exist "%~dp0\expanded\tools\python.exe" (echo Fatal expanded\tools\python.exe not found & pause) else (title %~f0 & "%~dp0\expanded\tools\python.exe" "%~f0" %*)) || pause & exit /B & # noqa: E501
 def __unused(): "fake function to help writing header that allows executing same file as python and batch"
 
-import sys, pathlib, shutil, zipfile
+import sys, pathlib, shutil, zipfile, subprocess
+SELF_DIR = pathlib.Path(__file__).absolute().parent
 
 def create_python_distr(full_dir: pathlib.Path, target_dir: pathlib.Path):
     if target_dir.exists():
@@ -11,14 +12,23 @@ def create_python_distr(full_dir: pathlib.Path, target_dir: pathlib.Path):
         if f.suffix.lower() in {".exe", ".dll"}:
             shutil.copy2(f, target_dir)
     expected_zip_name = pathlib.Path(next(p for p in sys.path if p.endswith(".zip"))).name
-    std_lib = zipfile.PyZipFile(target_dir / expected_zip_name, mode="w")
-    source_lib = full_dir/"Lib"
-    std_lib.writepy(source_lib)
-    for package in source_lib.iterdir():
-        if package.is_dir():
-            std_lib.writepy(package)
+    with zipfile.PyZipFile(target_dir / expected_zip_name, mode="w") as std_lib:
+        source_lib = full_dir/"Lib"
+        std_lib.writepy(source_lib)
+        target_site = target_dir/"Lib/site-packages"
+        target_site.mkdir(parents=True)
+
+        for package in source_lib.iterdir():
+            if package.is_dir():
+                if package.name in {"venv", "pydoc_data", "ensurepip"}:
+                    # packages with non-py files that a simpler to handle out-of-zip
+                    shutil.copytree(package, target_dir/"Lib"/package.name)
+                else:
+                    std_lib.writepy(package)
+    shutil.copy2(SELF_DIR / "python3.pth", target_dir)
+    subprocess.check_call([str(target_dir/pathlib.Path(sys.executable).name), "-m", "ensurepip", "--default-pip"], cwd=target_dir)
 
 if __name__ == "__main__":
     full_dir = pathlib.Path(sys.executable).absolute().parent
-    target_dir = pathlib.Path(__file__).absolute().parent / "artifacts"
+    target_dir = SELF_DIR / "artifacts"
     create_python_distr(full_dir, target_dir)
