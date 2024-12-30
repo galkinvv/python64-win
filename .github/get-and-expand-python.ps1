eea4a4b8d2dbf38a1d491efd5dd1a84b84577328
Vasily Galkin <galkin.vv@yandex.kz>
2024-12-30 23:58:30 +0300

    avoid logs in expanded

--- .github/get-and-expand-python.ps1
+++ .github/get-and-expand-python.ps1
@@ -2,10 +2,9 @@ param (
     [Parameter(Mandatory=$true)][string]$pythoninstexe,
     [Parameter(Mandatory=$true)][string]$pythoninsturl
 )
-New-Item -Type Directory -Force ./downloaded
 if (!(Test-Path "./downloaded/${pythoninstexe}")) {
+    New-Item -Type Directory -Force ./downloaded
     Invoke-WebRequest -Uri "${pythoninsturl}/${pythoninstexe}" -OutFile "./downloaded/${pythoninstexe}"
 }
-New-Item -Type Directory -Force ./expanded
 #run exe and wauit for its completion
-& "downloaded/${pythoninstexe}" /quiet /log  expanded/install-log.txt AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 "DefaultJustForMeTargetDir=${PSScriptRoot}\expanded" | Out-Default
+& "downloaded/${pythoninstexe}" /quiet /log  ${PSScriptRoot}/expanded/install-log.txt AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 "DefaultJustForMeTargetDir=${PSScriptRoot}\expanded" | Out-Default
