3f887bdbc49716f9d9f8a6f88633f377c9734b72
Vasily Galkin <galkin.vv@yandex.kz>
2024-12-30 03:35:55 +0300

    try full installer

--- get-and-expand-python.ps1
+++ get-and-expand-python.ps1
@@ -6,6 +6,10 @@ New-Item -Type Directory -Force ./downloaded
 if (!(Test-Path "./downloaded/python.${pythonver}.nupkg.zip")) {
     Invoke-WebRequest -Uri "https://github.com/adang1345/PythonWin7/raw/refs/heads/master/${pythonver}/python.${pythonver}.nupkg" -OutFile "./downloaded/python.${pythonver}.nupkg.zip"
 }
-Expand-Archive -LiteralPath "./downloaded/python.${pythonver}.nupkg.zip"
-Rename-Item "python.${pythonver}.nupkg" -NewName "expanded"
+if (!(Test-Path "./downloaded/python-${pythonver}-amd64-full.exe")) {
+    Invoke-WebRequest -Uri "https://github.com/adang1345/PythonWin7/raw/refs/heads/master/${pythonver}/python-${pythonver}-amd64-full.exe" -OutFile "./downloaded/python-${pythonver}-amd64-full.exe"
+}
+# Expand-Archive -LiteralPath "./downloaded/python.${pythonver}.nupkg.zip"
+# Rename-Item "python.${pythonver}.nupkg" -NewName "expanded"
+downloaded/python-${pythonver}-amd64-full.exe /passive AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 DefaultJustForMeTargetDir=expanded
 Get-ChildItem "expanded"
