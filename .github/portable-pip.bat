8b3027100a7350e13aac69aef02d5c514556e156
Vasily Galkin <galkin.vv@yandex.kz>
2024-12-31 01:05:43 +0300

    fixup pip behaviour on autoupdate

--- .github/portable-pip.bat
+++ .github/portable-pip.bat
@@ -1,6 +1,6 @@
 @PATH %~dp0;%PATH%
 @FOR %%w IN ("%~dp0\Lib\ensurepip\_bundled\*.whl") DO @SET PYTHONPATH=%%w;%PYTHONPATH%
-@"%~dp0\python" -c "import sys,sysconfig,os.path,os,runpy; sys.argv[0]=r'%~n0'"^
+@"%~dp0\python" -c "import sys,sysconfig,os.path,os,runpy; sys.argv[0]=r'%~n0.bat'"^
 
 "os.chdir(os.path.dirname(sys.executable)); sys.executable=r'.\python.exe'  # alters the python name embedded in installed launchers"^
 
--- ConsolePIPinZIP.bat
+++ ConsolePIPinZIP.bat
@@ -1,4 +1,6 @@
 @PATH %~dp0;%PATH%
 @PROMPT %CONSOLE_NAME_PLACEHOLDER% %PROMPT%
+:: prefer pip.bat over pip.exe
+@SET PATHEXT=.BAT;%PATHEXT%
 @FOR %%w IN ("%~dp0\Lib\ensurepip\_bundled\*.whl") DO @SET PYTHONPATH=%%w
 @start "%CONSOLE_NAME_PLACEHOLDER% from %~dp0" cmd /k "<nul set /p=Type ^"python^" to start interpreter or add packages via:   pip install package_name"
