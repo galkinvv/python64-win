4b63a3b23bfe540796b3dc3b31155c8bbdb55c38
Vasily Galkin <galkin.vv@yandex.kz>
2024-12-30 04:18:51 +0300

    publish intermediate artifact

--- get-and-expand-python.ps1
+++ get-and-expand-python.ps1
@@ -9,7 +9,8 @@ if (!(Test-Path "./downloaded/python.${pythonver}.nupkg.zip")) {
 if (!(Test-Path "./downloaded/python-${pythonver}-amd64-full.exe")) {
     Invoke-WebRequest -Uri "https://github.com/adang1345/PythonWin7/raw/refs/heads/master/${pythonver}/python-${pythonver}-amd64-full.exe" -OutFile "./downloaded/python-${pythonver}-amd64-full.exe"
 }
+New-Item -Type Directory -Force "expanded"
 # Expand-Archive -LiteralPath "./downloaded/python.${pythonver}.nupkg.zip"
 # Rename-Item "python.${pythonver}.nupkg" -NewName "expanded"
-& "downloaded/python-${pythonver}-amd64-full.exe" /quiet AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 DefaultJustForMeTargetDir=expanded | Out-Default
+& "downloaded/python-${pythonver}-amd64-full.exe" /quiet /log  expanded/install-log.txt AssociateFiles=0 Include_doc=0 Include_dev=0 Include_launcher=0 Include_pip=0 Include_test=0 Shortcuts=0 DefaultJustForMeTargetDir=expanded | Out-Default
 Get-ChildItem "expanded"
