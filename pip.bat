5e8a51bd032486bb3c58dc51a98fe0672871eba7
Vasily Galkin <galkin.vv@yandex.kz>
2025-01-01 02:59:29 +0300

    make pip.bat handle Ctrl+c without extra "terminate batch job" question

--- pip.bat
+++ pip.bat
@@ -1,13 +1,11 @@
-@"%~dp0\python" -m _pip_in_zip %~n0.bat %*
-:: Check if script was executed by double clock in explorer - search for script name near the end of cmdcmdline
-@ECHO %cmdcmdline%| findstr /I /R %~n0.bat...$ >NUL
-@IF %ERRORLEVEL%==0 (
-    ECHO. 
-    ECHO. 
-    ECHO Note: %~n0.bat is NOT intended to be run from GUI
+:: presence of following "|| call call" supresses "Terminate batch job" question on Ctrl+C
+@"%~dp0\python" -m _pip_in_zip %~n0.bat %* || CALL CALL
+@IF /I "%COMSPEC% /c %~f0 " EQU "%cmdcmdline:"=%" (
+    :: script name with a trailing space present at the end of cmdcmdline - so script was executed by double click in explorer
+    ECHO.
+    powershell Write-Host -NoNewline -Back Red NOTE!
+    ECHO  %~nx0 is NOT intended to be run from GUI
     ECHO       Use ConsolePIPinZIP.bat to get terminal and enter command like below:
     ECHO       pip install package_name
-    ECHO.
-    ECHO       Press any key to continue...
-    PAUSE >NUL
+    PAUSE
 )
