1f837f74b03408feadd40c6e5546825bcc1a4611
galkinvv <galkinvv@users.noreply.github.com>
2025-01-01 00:03:09 +0000

    Github action build win64-3.12.8

--- ConsolePIPinZIP.bat
+++ ConsolePIPinZIP.bat
@@ -1,6 +1,6 @@
 @CD "%~dp0"
 @PATH %~dp0;%PATH%
-@PROMPT %CONSOLE_NAME_PLACEHOLDER% %PROMPT%
+@PROMPT PIPinZIP win64-3.12.8 %PROMPT%
 :: prefer pip.bat over pip.exe
 @SET PATHEXT=.BAT;%PATHEXT%
-@start "%CONSOLE_NAME_PLACEHOLDER% from %~dp0" cmd /k "<nul set /p=Type ^"python^" to start interpreter or add packages by:  pip install package_name"
+@start "PIPinZIP win64-3.12.8 from %~dp0" cmd /k "<nul set /p=Type ^"python^" to start interpreter or add packages by:  pip install package_name"
