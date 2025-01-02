79f969d8f5039bf657fe0e88cf76a870f9cc57a0
Vasily Galkin <galkin.vv@yandex.kz>
2025-01-02 05:20:56 +0300

    more robust terminate bath job suppressing

--- pip.bat
+++ pip.bat
@@ -1,5 +1,5 @@
-:: presence of following "|| call call" supresses "Terminate batch job" question on Ctrl+C
-@"%~dp0\python" -m _pip_in_zip %~n0.bat %* || CALL CALL
+:: presence of following "|| CALL IF EnsureError" supresses "Terminate batch job" question on Ctrl+C
+@"%~dp0\python" -m _pip_in_zip %~n0.bat %* || CALL IF EnsureError
 @IF /I "%COMSPEC% /c %~f0 " EQU "%cmdcmdline:"=%" (
     :: script name with a trailing space present at the end of cmdcmdline - so script was executed by double click in explorer
     ECHO.
